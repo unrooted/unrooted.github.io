fd8fedcce58dfe9323f1140f4935f81af80ee214
unrooted <konrad.root.klawikowski@gmail.com>
2026-06-07 05:13:41 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         hugo = pkgs.hugo;
@@ -15,10 +21,13 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "unrooted-blog";
-          version =
-            if self ? rev then builtins.substring 0 7 self.rev else "dirty";
+          version = if self ? rev then builtins.substring 0 7 self.rev else "dirty";
           src = self;
-          nativeBuildInputs = [ hugo pkgs.dart-sass pkgs.go ];
+          nativeBuildInputs = [
+            hugo
+            pkgs.dart-sass
+            pkgs.go
+          ];
           buildPhase = ''
             runHook preBuild
             export HUGO_CACHEDIR="$TMPDIR/hugo-cache"
@@ -31,26 +40,65 @@
         packages.site = self.packages.${system}.default;
 
         devShells.default = pkgs.mkShell {
-          packages = [ hugo pkgs.dart-sass pkgs.go pkgs.nixpkgs-fmt ];
+          packages = [
+            hugo
+            pkgs.dart-sass
+            pkgs.go
+            pkgs.nixfmt
+          ];
         };
 
         apps.serve = {
           type = "app";
-          program = toString (pkgs.writeShellScript "serve" ''
-            exec ${hugo}/bin/hugo server -D --bind 0.0.0.0 \
-              --noBuildLock --renderToMemory "$@"
-          '');
+          program = toString (
+            pkgs.writeShellScript "serve" ''
+              exec ${hugo}/bin/hugo server -D --bind 0.0.0.0 \
+                --noBuildLock --renderToMemory "$@"
+            ''
+          );
         };
 
         apps.update-theme = {
           type = "app";
-          program = toString (pkgs.writeShellScript "update-theme" ''
-            export PATH=${pkgs.lib.makeBinPath [ hugo pkgs.go pkgs.git ]}:$PATH
-            hugo mod get -u
-            hugo mod vendor
-          '');
+          program = toString (
+            pkgs.writeShellScript "update-theme" ''
+              export PATH=${
+                pkgs.lib.makeBinPath [
+                  hugo
+                  pkgs.go
+                  pkgs.git
+                ]
+              }:$PATH
+              hugo mod get -u
+              hugo mod vendor
+            ''
+          );
         };
 
-        formatter = pkgs.nixpkgs-fmt;
-      });
+        formatter = pkgs.writeShellApplication {
+          name = "nixfmt-all";
+          runtimeInputs = [
+            pkgs.fd
+            pkgs.nixfmt
+          ];
+          text = ''
+            fd "$@" -t f -e nix -E _vendor -x nixfmt '{}'
+          '';
+        };
+
+        checks.formatting =
+          pkgs.runCommand "check-formatting"
+            {
+              nativeBuildInputs = [
+                pkgs.fd
+                pkgs.nixfmt
+              ];
+            }
+            ''
+              cd ${self}
+              fd . -t f -e nix -E _vendor -x nixfmt --check '{}'
+              touch $out
+            '';
+      }
+    );
 }
